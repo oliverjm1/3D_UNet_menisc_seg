7ee73c34ada5b645441f2e928098e4dcc3a4e386
oliverjm1 <75314670+oliverjm1@users.noreply.github.com>
2023-08-10 17:12:13 +0100

    alter dice score/loss to make generalisable across 2/3D

--- src/metrics.py
+++ src/metrics.py
@@ -9,23 +9,24 @@ def dice_coefficient(mask1, mask2):
     return dice.item()
 
 # Return average dice coeff for a batch of input and target masks
-def batch_dice_coeff(outputs, targets):
-    # inputs/targets both have 5 dims (spatial are final 3)
-    intersection = torch.sum(outputs * targets, dim=(2,3,4))
-    sum = torch.sum(outputs, dim=(2,3,4)) + torch.sum(targets, dim=(2,3,4))
-    dice = (2.0 * intersection) / sum
+# 'smooth' constant included to avoid NaN errors when volume is zero
+def batch_dice_coeff(input, target, smooth=1e-5):
+    # get tuple of spatial dimensions (first two dims are batch and channel)
+    spatial_dims = tuple(range(2, len(input.shape)))
+    
+    # calculate intersection & sum of masks, then calculate dice coeff
+    intersection = torch.sum(input * target, dim=spatial_dims)
+    sum = torch.sum(input, dim=spatial_dims) + torch.sum(target, dim=spatial_dims)
+    dice = (2.0 * intersection + smooth) / (sum + smooth)
+
+    # return mean dice coeff of batch
     return torch.mean(dice)
 
 # Define Dice Loss
-# This is 1 - dice coeff. Need to be wary of batch/channel dims.
-# Calculate dice loss for each item in batch and average.
-def dice_loss(outputs, targets):
-    # inputs/targets both have 5 dims (spatial are final 3)
-    intersection = torch.sum(outputs * targets, dim=(2,3,4))
-    sum = torch.sum(outputs, dim=(2,3,4)) + torch.sum(targets, dim=(2,3,4))
-    dice = (2.0 * intersection) / sum
-    loss = 1 - dice
-    return torch.mean(loss)
+# This is 1 - dice coeff.
+def dice_loss(input, target):
+    mean_loss = 1 - batch_dice_coeff(input, target)
+    return mean_loss
 
 # Loss that includes both binary cross-entropy and dice loss
 def bce_dice_loss(outputs, targets):
